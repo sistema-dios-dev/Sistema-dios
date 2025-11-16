444ed3fb01323ac7bc4e400ccb4a01081108faea
sistema-dios-dev <fabiannavillegas10@gmail.com>
2025-11-15 23:03:56 -0400

    Update manage.py

--- manage.py
+++ manage.py
@@ -1,11 +1,12 @@
-7#!/usr/bin/env python
+#!/usr/bin/env python
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
 
+
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_dashboard.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -15,6 +16,7 @@ def main():
             "forget to activate a virtual environment?"
         ) from exc
     execute_from_command_line(sys.argv)
+
 
 if __name__ == '__main__':
     main()
