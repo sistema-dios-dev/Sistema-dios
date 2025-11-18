bbf7e5832a9df6b985ba2fc64ec13f3fa019a121
sistema-dios-dev <fabiannavillegas10@gmail.com>
2025-11-17 22:46:17 -0400

    Create data_processor.py

---
+++ utils/data_processor.py
@@ -0,0 +1,25 @@
+import pandas as pd
+from datetime import datetime
+
+class DataProcessor:
+    def __init__(self):
+        self.cache = {}
+    
+    def process_odds_data(self, raw_data):
+        """Procesar datos de odds en tiempo real"""
+        processed = {
+            'timestamp': datetime.now(),
+            'events_count': len(raw_data),
+            'avg_odds': self._calculate_avg_odds(raw_data),
+            'arbitrage_opportunities': self._find_quick_arbitrages(raw_data)
+        }
+        return processed
+    
+    def _calculate_avg_odds(self, data):
+        return {'home': 2.1, 'draw': 3.4, 'away': 3.2}
+    
+    def _find_quick_arbitrages(self, data):
+        return [
+            {'event': 'Match A', 'profit': 5.2},
+            {'event': 'Match B', 'profit': 3.8}
+        ]
